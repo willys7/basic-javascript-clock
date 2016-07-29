df8e7d8a4c53716c97407d5bf73893294ed6f5dd
William Funtes <William Funtes>
2016-07-29 00:25:35 -0600

    reloj digital

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,61 +1,77 @@
-/*
-
-state = 0: red
-state = 1: yellow
-state = 2: green
-
-*/
 
 // Modelo / estado
+//Mi estado esta representado por
+// la hora el minuto el segundo y el tipo de reloj
+// que tenemos en las variables iniciales se puede elejir 
+//la hora con la que se desea que comience el reloj
 var state = 0;
+var hora1=0;
+var hora2=0;
+var minuto1=0;
+var minuto2=0;
+var segundo=1;
 
-var viewport = document.getElementById("viewport");
-var changeTrigger = document.getElementById("change");
 
-changeTrigger.addEventListener("click", function(){
-  if(state === 0){
-    state = 2;
-  }
-  else if(state === 1){
-    state = 0;
-  }
-  else{
-    state = 1;
-  }
+var canvas=document.getElementById("reloj");
 
-  viewport.innerHTML = render(state);
+var clockController= document.getElementById("tipo");
+
+clockController.addEventListener("change", function() {
+    if (clockController.value=== "digital") {
+      state=0;
+    }else if(clockController.value === "analogo"){
+      state=1;
+    }
+    canvas.innerHTML = render2(state);
 });
 
+var changeTrigger = document.getElementById("change");
 
-function render(state){
+function render2(state){
   var html = "";
+  if (state==0) {
+    html += '<div class="display">';
+    html += '<div class="numero H1">'+ hora1 +'</div>';
+    html += '<div class="numero H2">'+ hora2 +'</div>';
+    html += '<div class="numero M1">'+ ': '+ minuto1 +'</div>';
+    html += '<div class="numero M2">'+ minuto2 +'</div>';
+    html += '<div class="segundos">'+ ':'+segundo +'</div>';
+    html += '<div style="clear:both;"></div>';
+    html += '</div>';
 
-  html += '<div class="traffic-light">';
+    segundo+=1;
+    if(segundo===60){
+      segundo=0;
+      minute2+=1;
+    }
+    if(minuto2>9){
+      minuto1+=1;
+      minuto2=0;
+    }
+    if (minuto1>5) {
+      hora2+=1;
+      minuto1=0;
+      minuto2=0;
+    }
+    if (hora1===2) {
+      if (hora2>3) {
+        hora2=0;
+        hora1=0;
+        minuto2=0;
+        minuto1=0;
+      }
+    }
 
-  if(state === 0){
-    html += '<div class="red light"></div>';
   }
-  else{
-    html += '<div class="red light off"></div>';
-  }
+  else if (state===1) {
 
-  if(state === 1){
-    html += '<div class="yellow light"></div>';
+    
   }
-  else{
-    html += '<div class="yellow light off"></div>';
-  }
-
-  if(state === 2){
-    html += '<div class="green light"></div>';
-  }
-  else{
-    html += '<div class="green light off"></div>';
-  }
-
-  html += '</div>';
-
-  return html;
+  return html; 
 }
 
-viewport.innerHTML = render(state);
+function Iniciar_reloj(){
+  canvas.innerHTML=render2(state);
+}
+Iniciar_reloj();
+setInterval(Iniciar_reloj,1000);
